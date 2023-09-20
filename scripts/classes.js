862ef227e7b1a2249c30eeebc3a64d2cd5e1dbd2
BUNJEE <vsolomatin@kse.org.ua>
2023-09-20 16:05:07 +0300

    linked book and bookshelf

--- scripts/classes.js
+++ scripts/classes.js
@@ -14,17 +14,59 @@ class Book{
     }
 }
 
+class Bookshelf{
+    constructor(books) {
+        this.books = [];
+    }
+    addBook(book){
+        this.books.push(book);
+    }
+
+    removeBook(book){
+        const indexToRemove = this.books.findIndex(book => (
+            book.title === bookToRemove.title &&
+            book.authors === bookToRemove.authors &&
+            book.numberOfPages === bookToRemove.numberOfPages
+        ));
+
+        if (indexToRemove !== -1) {
+            this.books.splice(indexToRemove, 1);
+            console.log("Book removed from the bookshelf.");
+        } else {
+            console.log("Book not found in the bookshelf.");
+        }
+    }
+
+    getUnreadBooks() {
+        const unreadBooks = this.books.filter(book => !book.isRead);
+        return unreadBooks;
+    }
+
+    getFavBooks(){
+        const unreadBooks = this.books.filter(book => !book.isFavorite);
+        return unreadBooks;
+    }
+}
 
 
+let bookInstance1 = new Book("LalaLend", "Brown, Tim del Ray", 200, false, false);
+let bookInstance2 = new Book("LalaLend2", "Brown1, Tim del Ray1", 250, false, false);
+console.log(bookInstance1);
 
-// let bookInstance = new Book("LalaLend", "Brown, Tim del Ray", 200, false, false);
-// console.log(bookInstance);
+// bookInstance1.markAsRead();
+// console.log(bookInstance1.isRead); // Should be true
 //
-// bookInstance.markAsRead();
-// console.log(bookInstance.isRead); // Should be true
+// bookInstance1.toggleFavorite();
+// console.log(bookInstance1.isFavorite); // Should be true
 //
-// bookInstance.toggleFavorite();
-// console.log(bookInstance.isFavorite); // Should be true
-//
-// bookInstance.toggleFavorite();
-// console.log(bookInstance.isFavorite); // Should be false
+// bookInstance1.toggleFavorite();
+// console.log(bookInstance1.isFavorite); // Should be false
+
+// Create an instance of the Bookshelf class
+const myBookshelf = new Bookshelf();
+
+// Add the book instances to the bookshelf
+myBookshelf.addBook(bookInstance1);
+myBookshelf.addBook(bookInstance2);
+
+console.log(myBookshelf.books);
